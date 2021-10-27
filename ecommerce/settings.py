fc350784e47cfc4866354e80487c94885b250eab
siegfried <81885507+Fernando0313@users.noreply.github.com>
2021-10-26 21:09:58 -0500

    modificacion de db

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -81,19 +81,32 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': environ.get('DB_NAME'),
-        'USER': environ.get('DB_USER'),
-        'PASSWORD': environ.get('DB_PASSWORD'),
-        'PORT': environ.get('DB_PORT'),
-        'HOST': environ.get('DB_HOST'),
-         'TEST': {
-            'NAME': environ.get('DB_TEST')
+if environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github-actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432'
         }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': environ.get('DB_NAME'),
+            'USER': environ.get('DB_USER'),
+            'PASSWORD': environ.get('DB_PASSWORD'),
+            'PORT': environ.get('DB_PORT'),
+            'HOST': environ.get('DB_HOST'),
+            'TEST': {
+                'NAME': environ.get('DB_TEST')
+            }
+        }
+    }
+
 
 
 
