b495ea1dde4cb93328dfd2ecd5dfe79b3c537f1e
fernando0313 <rojofernando296@gmail.com>
2021-10-26 21:22:57 -0500

    aea

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -83,6 +83,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
